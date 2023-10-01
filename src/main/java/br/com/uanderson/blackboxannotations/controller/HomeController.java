9c3aa35b0da4fa50918003a45367eb04a86969cf
Uanderson Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2023-10-01 12:49:16 -0300

    commit

--- src/main/java/br/com/uanderson/blackboxannotations/controller/HomeController.java
+++ src/main/java/br/com/uanderson/blackboxannotations/controller/HomeController.java
@@ -2,9 +2,7 @@ package br.com.uanderson.blackboxannotations.controller;
 
 import br.com.uanderson.blackboxannotations.util.DateUtil;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.thymeleaf.ITemplateEngine;
@@ -13,13 +11,11 @@ import org.thymeleaf.web.IWebExchange;
 
 import java.io.Writer;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.Calendar;
 
 @Controller
 @RequestMapping(path = {"/"})
 @RequiredArgsConstructor
-@Log4j2
 public class HomeController {
 
     private final DateUtil dateUtil;
@@ -42,21 +38,5 @@ public class HomeController {
         return "fragments/_layout";
     }
 
-
-//    public void process(
-//            final IWebExchange webExchange,
-//            final ITemplateEngine templateEngine,
-//            final Writer writer)
-//            throws Exception {
-//
-//        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy");
-//        Calendar cal = Calendar.getInstance();
-//
-//        WebContext webContext = new WebContext(webExchange, webExchange.getLocale());
-//        webContext.setVariable("today", dateFormat.format(cal.getTime()));
-//
-//        templateEngine.process("fragments/_menuTop", webContext, writer);
-//
-//    }
 
 }//class
